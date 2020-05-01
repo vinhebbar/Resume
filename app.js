29cd1408db408f72d0c110c16d160d7843a57005
Vinayak Hebbar <vinvhebbar@gmail.com>
2020-05-01 11:55:40 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 function fd() {
-  v = document.getElementById("fd1");
+  var v = document.getElementById("fd1");
 
   if (v.value.trim() == "") {
     alert("Feedback cannot be empty");
   } else {
-    v = document.getElementById("fd1").value.split(" ").join("+");
     c = confirm(
-      "Your feedback will be submitted through WhatsApp Press OK to Confirm"
+      "Your feedback will be submitted through WhatsApp, Press OK to Confirm"
     );
 
     if (c == true) {
-      url = "https://wa.me/919900991692?text=" + v;
-      window.open(url, "_blank");
-     }
-    else {
-       alert('Feedback not submitted')
-     }
+      url = "https://wa.me/919900991692?text=" + encodeURIComponent(v.value);
+      window.open(url);
+    } else {
+      alert("Feedback not submitted");
+    }
   }
 }
